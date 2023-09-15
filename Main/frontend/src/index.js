b7ead08e202239864532b9da48149dece08e8507
Nikita Shah <202101209@daiict.ac.in>
2023-09-15 22:07:56 +0530

    react-BS added

--- Main/frontend/src/index.js
+++ Main/frontend/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css'  // from bootstrap lib
+import 'bootstrap/dist/css/bootstrap.min.css';  // from react-bootstrap lib
 import 'bootstrap/dist/js/bootstrap.bundle.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
