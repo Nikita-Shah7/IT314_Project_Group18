da35306913fb47b4ff19f4c975899a9b2e37e4a8
MaHARSHi1612 <maharshi161203@gmail.com>
2023-11-06 14:37:23 +0530

    Header added in components

---
+++ Main/frontend/src/components/login/header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function header()
+{
+    <div className="nav">
+        <h2>Restaurant</h2>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Menu</a></li>
+            <li><a href="">Events</a></li>
+            <li><a href="">Gallery</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+        <button className='btn btn-primary'>LogIn/SignUp</button>
+    </div>
+}
