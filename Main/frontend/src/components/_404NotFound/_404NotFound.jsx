25e1bf0348e5d8c0d30217902171a4dedbbdf189
Jahnavi-28 <94967779+Jahnavi-28@users.noreply.github.com>
2023-11-07 22:57:02 +0530

    404 added

---
+++ Main/frontend/src/components/_404NotFound/_404NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function _404NotFound() {
+    return(
+    <div className= "error-page" height={350}
+    width={700} align="center" top="50%" > <div className="centered-div"> <img src="/Images/404imagee.png" /> </div></div>
+
+        
+    );
+}
+
+export default _404NotFound;
