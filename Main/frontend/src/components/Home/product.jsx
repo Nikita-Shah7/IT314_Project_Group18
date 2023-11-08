95a31d0a4ee3777befdb6e63a124cd891854196b
Devang Rathod <rathoddev7226@gmail.com>
2023-11-08 18:52:04 +0530

    added new functionality in product.jsx
    Update product.jsx

--- Main/frontend/src/components/Home/product.jsx
+++ Main/frontend/src/components/Home/product.jsx
@@ -1,24 +1,71 @@
-import React, { useContext } from "react";
-import { ShopContext } from "../../context/shop-context";
+import React, { useContext, useState } from "react";
+import { ShopContext } from "../../context/shop-context"; // Import shop context
 
 export const Product = (props) => {
-  const { id, productName, price, productImage } = props.data;
-  const { addToCart, cartItems } = useContext(ShopContext);
-
+  const { id, productName, price, productImage, description, category } =
+    props.data;
+  const { addToCart, cartItems } = useContext(ShopContext); // Access the shop context
   const cartItemCount = cartItems[id];
 
+  // popup
+  const [isPopupOpen, setPopupOpen] = useState(false);
+  const togglePopup = () => {
+    setPopupOpen(!isPopupOpen);
+  };
+
   return (
-    <div className="product">
-      <img src={productImage} />
-      <div className="description">
-        <p  style={{marginTop: '5px'}}>
-          <b>{productName}</b>
-        </p>
-        <p  style={{marginTop: '1px', marginBottom: '5px'}}> ₹{price}</p>
+    <div>
+      <div className="individual-product" key={id}>
+        <img
+          className="product-image"
+          src={productImage}
+          alt={productName}
+          onClick={togglePopup}
+        />{" "}
+        <div className="description">
+          <p style={{ marginTop: "5px" }}>
+            <b>{productName}</b>
+          </p>
+          <p style={{ marginTop: "1px", marginBottom: "5px" }}>₹{price}</p>
+        </div>
+        <div className="cartbutton">
+          <button className="addToCartBttn" onClick={() => addToCart(id)}>
+            Add To Cart {cartItemCount > 0 && <> ({cartItemCount})</>}
+          </button>
+        </div>
       </div>
-      <button className="addToCartBttn" onClick={() => addToCart(id)}>
-        Add To Cart {cartItemCount > 0 && <> ({cartItemCount})</>}
-      </button>
+
+      {isPopupOpen && (
+        <div className="popup-container">
+          <div className="popup">
+            <div className="popup-content ">
+              <div className="popup-image">
+                <img src={productImage} alt={productName} />
+              </div>
+              <div className="popup-sidebar">
+                <h2>{productName}</h2>
+                <p>{description}</p>
+                <h3>
+                  <b>Price : ₹{price}</b>
+                </h3>
+              </div>
+            </div>
+            <div className="popup-bottom">
+              <div>
+                <button className="addToCartBttn" onClick={() => addToCart(id)}>
+                  Add to Cart
+                  {cartItemCount > 0 && <> ({cartItemCount})</>}
+                </button>
+              </div>
+              <div>
+                <button className="popupbutton" onClick={togglePopup}>
+                  Close
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
