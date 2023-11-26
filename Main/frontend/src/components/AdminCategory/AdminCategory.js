d76003a9f163dc842a956886afcc7f391f96408f
Nikita Shah <202101209@daiict.ac.in>
2023-11-26 16:17:08 +0530

    category-backend-and-axios-added

--- Main/frontend/src/components/AdminCategory/AdminCategory.js
+++ Main/frontend/src/components/AdminCategory/AdminCategory.js
@@ -1,38 +1,39 @@
 import React,{ useState, useEffect } from 'react'
 import { useNavigate } from "react-router-dom";
 import axios from 'axios';
+import {category as categoryAxios} from '../AxiosCreate';
 import './AdminCat.css'
 import Category from './category';
-import data from './data';
 
 
 export default function AdminCategory() {
 
-    // // console.log("nik in admin menu");
-    // const navigate = useNavigate();
+    // console.log("nik in admin category");
+    const navigate = useNavigate();
 
-    // if(!localStorage.getItem("isAdminAuth")) {
-    //     navigate('/adminlogin');
-    // }
+    if(!localStorage.getItem("isAdminAuth")) {
+        navigate('/adminlogin');
+    }
 
     const [loading, setLoading] = useState(true);
     const [category, setCategory] = useState([]);
+    const [categoryName, setCategoryName] = useState("");
     const [categoryCnt, setCategoryCnt] = useState(0);
 
-    // useEffect(() => {
-    //     setLoading(true)
-    //     axios.get(`http://localhost:5555/menu`)
-    //       .then((response) => {
-    //         // console.log([response.data][0].data)
-    //         setCartItemsCnt([response.data][0].count);
-    //         setCat([response.data][0].data)
-    //         setLoading(false);
-    //       })
-    //       .catch((error) => {
-    //         console.log("ERROR MESSAGE ::", error)
-    //         setLoading(false);
-    //       });
-    //   }, [menuItemsCnt]);
+    useEffect(() => {
+        setLoading(true)
+        categoryAxios.get('/')
+          .then((response) => {
+            // console.log([response.data][0].data)
+            setCategoryCnt([response.data][0].count);
+            setCategory([response.data][0].data)
+            setLoading(false);
+          })
+          .catch((error) => {
+            console.log("ERROR MESSAGE ::", error)
+            setLoading(false);
+          });
+      }, [categoryCnt]);
 
 
     const [modal,setModal] = useState(false);
@@ -42,51 +43,43 @@ export default function AdminCategory() {
     }
       
     
-    // const addItem = async(e) => {
-    //     e.preventDefault();
-    //     setLoading(true)
+    const addCategory = async(e) => {
+        e.preventDefault();
+        setLoading(true)
         
-    //     const accessToken = localStorage.getItem("accessToken");
-    //     // console.log(accessToken)
-    //     if(!accessToken)
-    //     {
-    //         setLoading(false);
-    //         // alert('An error happened. Please Check console');
-    //         // enqueueSnackbar('UNAUTHORIZED !!', { variant: 'error' });
-    //         console.log("UNAUTHORIZED!!");
-    //         return;
-    //     }
+        const accessToken = localStorage.getItem("accessToken");
+        // console.log(accessToken)
+        if(!accessToken)
+        {
+            setLoading(false);
+            // alert('An error happened. Please Check console');
+            // enqueueSnackbar('UNAUTHORIZED !!', { variant: 'error' });
+            console.log("UNAUTHORIZED!!");
+            return;
+        }
 
-    //     const data = {
-    //         categoryName: category,
-    //     }
-    //     await axios.post(`http://localhost:5555/menu`, data, {
-    //         headers: {
-    //             Authorization: `Bearer ${accessToken}`,
-    //         }
-    //     })
-    //         .then((response) => {
-    //         // console.log([response.data][0].data)
-    //         setCartItemsCnt(catCnt + 1);
-    //         setCategory("");
-    //         console.log([response.data][0].message);
-    //         setModal(!modal)
-    //         setLoading(false);
-    //         })
-    //         .catch((error) => {
-    //         console.log("ERROR MESSAGE ::", error)
-    //         setLoading(false);
-    //         });
-    // }
-    const allitem = data.map((item)=>{
-        return (
-            <Category
-            id = {item.id}
-            catname = {item.categoryName}
-            cnt = {item.count}
-            />
-        )
-    })
+        const data = {
+            categoryName: categoryName,
+        }
+        await categoryAxios.post('/', data, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+            }
+        })
+            .then((response) => {
+            // console.log([response.data][0].data)
+            setCategoryCnt(categoryCnt + 1);
+            setCategoryName("");
+            console.log([response.data][0].message);
+            setModal(!modal)
+            setLoading(false);
+            })
+            .catch((error) => {
+            console.log("ERROR MESSAGE ::", error)
+            setLoading(false);
+            });
+    }
+    
     return(
     <div className='adm-cat'>
         <div className="addi-cat">
@@ -95,22 +88,16 @@ export default function AdminCategory() {
                 <button className="but-list-cat" onClick={toggleModal}>Add Category</button>
             </div>
         </div>
-        <section className="item-list-cat">
-        {allitem}
-        {/* {products.map( (product) => (
-                <Cat key={product.menu_id} data={product} menuItemsCnt={menuItemsCnt} setMenuItemsCnt={setMenuItemsCnt} />
-              ))} */}
-        </section>
-        { modal && (
+        { modal ? (
             <div className='overlay-cat' onClick={toggleModal}>
                 <div className='content-ct' onClick={ (event) => event.stopPropagation()} >
-                <form className='mrow-cat'>
+                <form className='mrow-cat' onSubmit={addCategory}>
                 <div className="row-cat">
                     <div >
                         <label htmlFor="title" ><p>Category Name : </p></label>
                     </div>
                     <div>
-                    <input type="text" placeholder='Category Name' className='in-ca' name="categoryName" required value={category} onChange={(e) => setCategory(e.target.value)}/>
+                        <input type="text" className="in-ad" name="category" required value={categoryName} onChange={(e) => setCategoryName(e.target.value)} />
                     </div>
                 </div>
                 <div className='bu-fo-cat'>
@@ -119,7 +106,15 @@ export default function AdminCategory() {
                 </div>
             </form>
                 </div>
-        </div>)}    
+        </div>) : (
+            <section className="item-list">
+            {
+                category.map( (item) => (
+                    <Category key={item.category_id} data={item} categoryCnt={categoryCnt} setCategoryCnt={setCategoryCnt} />
+                  ))
+            }
+            </section>
+        )}    
         </div>
     )
 }
