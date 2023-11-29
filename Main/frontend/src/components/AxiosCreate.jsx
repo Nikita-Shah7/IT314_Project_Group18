3763c0795c92e41c32066324c394deeb89733a45
Nikita Shah <202101209@daiict.ac.in>
2023-11-29 21:58:48 +0530

    deployment-error-2

--- Main/frontend/src/components/AxiosCreate.jsx
+++ Main/frontend/src/components/AxiosCreate.jsx
@@ -1,11 +1,10 @@
 import axios from "axios";
-// import {API_URL} from '../config.js';
-require('dotenv').config();
+import {API_URL} from '../config.js';
 
 // const SERVER_HOST = "localhost";
 // const SERVER_PORT = 5555;
 // const SERVER_SOCKET_ADDRESS = "http://" + SERVER_HOST + ":" + SERVER_PORT + "/";
-const SERVER_SOCKET_ADDRESS = process.env.API_URL;
+const SERVER_SOCKET_ADDRESS = API_URL;
 
 
 export const category = axios.create({
