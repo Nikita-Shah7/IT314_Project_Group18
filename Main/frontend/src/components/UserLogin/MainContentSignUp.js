cc24e78a320abc8714df0ccf2363bdf90845d0da
MaHARSHi1612 <maharshi161203@gmail.com>
2023-11-29 20:37:40 +0530

    Signup page Updated with responsive header

--- Main/frontend/src/components/UserLogin/MainContentSignUp.js
+++ Main/frontend/src/components/UserLogin/MainContentSignUp.js
@@ -2,83 +2,84 @@ import React, { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-
-
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import { Link } from '@mui/material';
+import LoginIcon from '@mui/icons-material/Login';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
 
 const loginBoxStyle = {
-    width: '380px',
+    width: '90%',
+    maxWidth: '380px',
     padding: '20px',
     backgroundColor: '#f0f4d4',
     border: '2px solid #982c2c',
     borderRadius: '0',
     margin: '0 auto',
-    marginTop: '180px',
+    marginTop: '80px',
     marginBottom: '50px',
-    transform: 'translateY(-50%)',
 };
 
 const textFieldStyle = {
     backgroundColor: 'white',
-    width: '360px',
-    ml: '10px',
-    borderRadius: '0',
-
+    width: '100%',
+    marginBottom: '15px',
     '& .MuiInputBase-input': {
-        border: '1px solid #982c2c', // Default border color
+        border: '1px solid #982c2c',
         borderRadius: '4px',
     },
     '& :focus': {
-        border: '0px solid #982c2c', // Border color on focus 
+        border: '0px solid #982c2c',
         outline: 'none',
     },
 };
 
-const loginTextStyle = {
-    color: '#982c2c',
-    marginBottom: '85px',
-    marginTop: '190px'
+const centerDivStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    minHeight: '100vh',
 };
 
+const loginTextStyle = {
+    color: '#982c2c',
+    marginBottom: '20px',
+    textAlign: 'center',
+    justifyContent: 'center'
+};
 
-export default function LoginBox() {
+export default function SignUpBox() {
     const [name, setName] = useState('');
-    const [contactNumber, setContactNumber] = useState('');
     const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const [nameError, setNameError] = useState('');
+    const [emailError, setEmailError] = useState('');
 
     const validateEmail = (email) => {
-        // email validation
-        const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
-        return emailRegex.test(email);
+        const emailRegex = /^[A-Za-z0-9!#$%&'*+-/=?^_`{|}~]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+        const isValid = emailRegex.test(email);
+        setEmailError(isValid ? '' : 'Invalid email address');
+        return isValid;
     };
 
-    const validatePhoneNumber = (phoneNumber) => {
-        // 10-digit phone number validation
-        const phoneRegex = /^\d{10}$/;
-        return phoneRegex.test(phoneNumber);
+    const validateName = (name) => {
+        const hasNoNumbers = !/[0-9]/.test(name);
+        setNameError(hasNoNumbers ? '' : 'Name cannot contain numbers');
+        return hasNoNumbers;
     };
 
     const handleSubmit = () => {
-        // Validate email and phone number
-        if (!validateEmail(email)) {
-            alert('Invalid email address');
-            return;
-        }
+        const isNameValid = validateName(name);
+        const isEmailValid = validateEmail(email);
 
-        if (!validatePhoneNumber(contactNumber)) {
-            alert('Invalid phone number');
-            return;
+        if (isNameValid && isEmailValid) {
+            setName('');
+            setEmail('');
         }
-
-        // Resetting input values
-        setName('');
-        setContactNumber('');
-        setEmail('');
-        setPassword('');
     };
 
     return (
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100vh', justifyContent: 'center' }}>
+        <div style={centerDivStyle}>
             <Typography variant="h4" gutterBottom align='center' style={loginTextStyle}>
                 User Validation
             </Typography>
@@ -92,15 +93,7 @@ export default function LoginBox() {
                     sx={textFieldStyle}
                     onChange={(e) => setName(e.target.value)}
                 />
-                <TextField
-                    label="Enter Your Contact Number"
-                    variant="outlined"
-                    margin="normal"
-                    fullWidth
-                    sx={textFieldStyle}
-                    value={contactNumber}
-                    onChange={(e) => setContactNumber(e.target.value)}
-                />
+                {nameError && <Typography variant="caption" color="error">{nameError}</Typography>}
                 <TextField
                     label="Enter Your Email"
                     variant="outlined"
@@ -110,21 +103,7 @@ export default function LoginBox() {
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                 />
-                <TextField
-                    label="Enter Your Password"
-                    type="password"
-                    id="password"
-                    name="password"
-                    variant="outlined"
-                    margin="normal"
-                    autoComplete='="current-password
-                    required'
-                    fullWidth
-                    value={password}
-                    sx={textFieldStyle}
-                    onChange={(e) => setPassword(e.target.value)}
-
-                />
+                {emailError && <Typography variant="caption" color="error">{emailError}</Typography>}
                 <Button
                     variant="contained"
                     sx={{ width: '205px', backgroundColor: '#982c2c', margin: '0 auto', marginTop: '15px', marginBottom: '25px', display: 'block', borderRadius: '0' }}
@@ -133,6 +112,43 @@ export default function LoginBox() {
                     Submit
                 </Button>
 
+
+                <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', height: '60px' }}>
+                    <Box borderTop={1} borderColor="maroon" p={2} width={94} ml={3} mb={4} mt={6}>
+                        {/* Content for the first box */}
+                    </Box>
+                    <Typography variant="h6" sx={{ mx: 2, mb: 2 }}>
+                        OR
+                    </Typography>
+                    <Box borderTop={1} borderColor="maroon" p={2} width={90} mb={4} mt={6}>
+                        {/* Content for the second box */}
+                    </Box>
+                </div>
+
+
+                <Grid container spacing={1} justifyContent="center" alignItems="center">
+                    <Grid item>
+                        <span>Previously Visited ?</span>
+                    </Grid>
+                    <Grid item>
+                        <Link href="#" style={{ display: 'flex', alignItems: 'center' }}>
+                            <LoginIcon style={{ marginRight: '8px' }} />
+                            Login
+                        </Link>
+                    </Grid>
+                </Grid>
+
+                <Grid container spacing={1} justifyContent="center" alignItems="center">
+                    <Grid item>
+                        <span>Admin Login</span>
+                    </Grid>
+                    <Grid item>
+                        <Link href="#" style={{ display: 'flex', alignItems: 'center' }}>
+                            <SupervisorAccountIcon style={{ marginRight: '8px', marginTop: '8px' }} />
+                            Login
+                        </Link>
+                    </Grid>
+                </Grid>
             </div>
         </div>
     );
