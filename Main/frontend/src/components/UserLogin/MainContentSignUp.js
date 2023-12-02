4e57ceb93d70ece3945fdf0237321482d9fabfbf
MaHARSHi1612 <maharshi161203@gmail.com>
2023-12-02 15:10:21 +0530

    UserSignUp UI improved

--- Main/frontend/src/components/UserLogin/MainContentSignUp.js
+++ Main/frontend/src/components/UserLogin/MainContentSignUp.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import {useNavigate} from 'react-router-dom';
-import TextField from '@mui/material/TextField';
+import { useNavigate } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
@@ -17,22 +16,17 @@ const loginBoxStyle = {
     border: '2px solid #982c2c',
     borderRadius: '0',
     margin: '0 auto',
-    marginTop: '80px',
+    marginTop: '40px',
     marginBottom: '50px',
 };
 
-const textFieldStyle = {
+const inputStyle = {
     backgroundColor: 'white',
     width: '100%',
     marginBottom: '15px',
-    '& .MuiInputBase-input': {
-        border: '1px solid #982c2c',
-        borderRadius: '4px',
-    },
-    '& :focus': {
-        border: '0px solid #982c2c',
-        outline: 'none',
-    },
+    border: '1px solid #982c2c',
+    borderRadius: '4px',
+    padding: '10px',
 };
 
 const centerDivStyle = {
@@ -45,14 +39,13 @@ const centerDivStyle = {
 
 const loginTextStyle = {
     color: '#982c2c',
-    marginBottom: '20px',
+    marginTop: '100px',
+    marginBottom: '2px',
     textAlign: 'center',
     justifyContent: 'center'
 };
 
 export default function SignUpBox() {
-
-    const [loading, setLoading] = useState(true);
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [nameError, setNameError] = useState('');
@@ -86,56 +79,66 @@ export default function SignUpBox() {
     return (
         <div style={centerDivStyle}>
             <Typography variant="h4" gutterBottom align='center' style={loginTextStyle}>
-                User Login
+                User SignUp
             </Typography>
             <div style={loginBoxStyle}>
-                <TextField
-                    label="Enter Your Name"
-                    variant="outlined"
-                    margin="normal"
-                    fullWidth
+                <input
+                    type="text"
+                    placeholder="Enter Your Name"
+                    style={inputStyle}
                     value={name}
-                    sx={textFieldStyle}
                     onChange={(e) => setName(e.target.value)}
                 />
                 {nameError && <Typography variant="caption" color="error">{nameError}</Typography>}
-                <TextField
-                    label="Enter Your Email"
-                    variant="outlined"
-                    margin="normal"
-                    fullWidth
-                    sx={textFieldStyle}
+                <input
+                    type="text"
+                    placeholder="Enter Your Email"
+                    style={inputStyle}
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                 />
                 {emailError && <Typography variant="caption" color="error">{emailError}</Typography>}
-                <Button
-                    variant="contained"
-                    sx={{ width: '205px', backgroundColor: '#982c2c', margin: '0 auto', marginTop: '15px', marginBottom: '25px', display: 'block', borderRadius: '0' }}
-                    onClick={handleSubmit}
-                >
-                    Submit
-                </Button>
-                <Button
-                    variant="contained"
-                    sx={{ width: '205px', backgroundColor: '#982c2c', margin: '0 auto', marginTop: '15px', marginBottom: '20px', display: 'block', borderRadius: '0' }}
-                    onClick={() => navigate('/')}
-                >
-                    Back
-                </Button>
 
+                <Grid container spacing={2}>
+                    <Grid item xs={12} sm={6}>
+                        <Button
+                            variant="contained"
+                            fullWidth
+                            sx={{ backgroundColor: '#982c2c', borderRadius: '0' }}
+                            onClick={handleSubmit}
+                        >
+                            Submit
+                        </Button>
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <Button
+                            variant="contained"
+                            fullWidth
+                            sx={{ backgroundColor: '#982c2c', borderRadius: '0' }}
+                            onClick={() => navigate('/')}
+                        >
+                            Back
+                        </Button>
+                    </Grid>
+                </Grid>
 
-                <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', height: '60px' }}>
-                    <Box borderTop={1} borderColor="maroon" p={2} width={94} ml={3} mb={4} mt={6}>
-                        {/* Content for the first box */}
-                    </Box>
-                    <Typography variant="h6" sx={{ mx: 2, mb: 2 }}>
-                        OR
-                    </Typography>
-                    <Box borderTop={1} borderColor="maroon" p={2} width={90} mb={4} mt={6}>
-                        {/* Content for the second box */}
-                    </Box>
-                </div>
+                <Grid container justifyContent="center" alignItems="center" spacing={2} mt={4}>
+                    <Grid item>
+                        <Box borderTop={1} borderColor="maroon" p={2} width={{ xs: '80px', sm: '94px' }}>
+
+                        </Box>
+                    </Grid>
+                    <Grid item>
+                        <Typography variant="h6" sx={{ mb: 4 }}>
+                            OR
+                        </Typography>
+                    </Grid>
+                    <Grid item>
+                        <Box borderTop={1} borderColor="maroon" p={2} width={{ xs: '80px', sm: '90px' }}>
+
+                        </Box>
+                    </Grid>
+                </Grid>
 
 
                 <Grid container spacing={1} justifyContent="center" alignItems="center">
@@ -165,4 +168,5 @@ export default function SignUpBox() {
         </div>
     );
 }
+
 
