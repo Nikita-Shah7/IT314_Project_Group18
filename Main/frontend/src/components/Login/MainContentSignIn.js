d96291b5f0882be41faf104607df553c82c63f90
MaHARSHi1612 <maharshi161203@gmail.com>
2023-12-02 15:18:12 +0530

    Improved UI of User SignIn page

--- Main/frontend/src/components/Login/MainContentSignIn.js
+++ Main/frontend/src/components/Login/MainContentSignIn.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import TextField from '@mui/material/TextField';
+import { useNavigate } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
@@ -16,22 +16,17 @@ const loginBoxStyle = {
     border: '2px solid #982c2c',
     borderRadius: '0',
     margin: '0 auto',
-    marginTop: '80px',
+    marginTop: '40px',
     marginBottom: '50px',
 };
 
-const textFieldStyle = {
+const inputStyle = {
     backgroundColor: 'white',
     width: '100%',
     marginBottom: '15px',
-    '& .MuiInputBase-input': {
-        border: '1px solid #982c2c',
-        borderRadius: '4px',
-    },
-    '& :focus': {
-        border: '0px solid #982c2c',
-        outline: 'none',
-    },
+    border: '1px solid #982c2c',
+    borderRadius: '4px',
+    padding: '10px',
 };
 
 const centerDivStyle = {
@@ -44,7 +39,8 @@ const centerDivStyle = {
 
 const loginTextStyle = {
     color: '#982c2c',
-    marginBottom: '20px',
+    marginTop: '100px',
+    marginBottom: '2px',
     textAlign: 'center',
     justifyContent: 'center'
 };
@@ -52,6 +48,8 @@ const loginTextStyle = {
 export default function SignUpBox() {
     const [email, setEmail] = useState('');
     const [emailError, setEmailError] = useState('');
+
+    const navigate = useNavigate();
 
     const validateEmail = (email) => {
         const emailRegex = /^[A-Za-z0-9!#$%&'*+-/=?^_`{|}~]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
@@ -74,35 +72,58 @@ export default function SignUpBox() {
                 User Validation
             </Typography>
             <div style={loginBoxStyle}>
-                <TextField
-                    label="Enter Your Email"
-                    variant="outlined"
-                    margin="normal"
-                    fullWidth
-                    sx={textFieldStyle}
+                <input
+                    type="text"
+                    placeholder="Enter Your Email"
+                    style={inputStyle}
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                 />
                 {emailError && <Typography variant="caption" color="error">{emailError}</Typography>}
-                <Button
-                    variant="contained"
-                    sx={{ width: '205px', backgroundColor: '#982c2c', margin: '0 auto', marginTop: '15px', marginBottom: '25px', display: 'block', borderRadius: '0' }}
-                    onClick={handleSubmit}
-                >
-                    Submit
-                </Button>
 
-                <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', height: '60px' }}>
-                    <Box borderTop={1} borderColor="maroon" p={2} width={94} ml={3} mb={4} mt={6}>
-                        {/* Content for the first box */}
-                    </Box>
-                    <Typography variant="h6" sx={{ mx: 2, mb: 2 }}>
-                        OR
-                    </Typography>
-                    <Box borderTop={1} borderColor="maroon" p={2} width={90} mb={4} mt={6}>
-                        {/* Content for the second box */}
-                    </Box>
-                </div>
+
+                <Grid container spacing={2}>
+                    <Grid item xs={12} sm={6}>
+                        <Button
+                            variant="contained"
+                            fullWidth
+                            sx={{ backgroundColor: '#982c2c', borderRadius: '0' }}
+                            onClick={handleSubmit}
+                        >
+                            Submit
+                        </Button>
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <Button
+                            variant="contained"
+                            fullWidth
+                            sx={{ backgroundColor: '#982c2c', borderRadius: '0' }}
+                            onClick={() => navigate('/')}
+                        >
+                            Back
+                        </Button>
+                    </Grid>
+                </Grid>
+
+
+
+                <Grid container justifyContent="center" alignItems="center" spacing={2} mt={4}>
+                    <Grid item>
+                        <Box borderTop={1} borderColor="maroon" p={2} width={{ xs: '80px', sm: '94px' }}>
+
+                        </Box>
+                    </Grid>
+                    <Grid item>
+                        <Typography variant="h6" sx={{ mb: 4 }}>
+                            OR
+                        </Typography>
+                    </Grid>
+                    <Grid item>
+                        <Box borderTop={1} borderColor="maroon" p={2} width={{ xs: '80px', sm: '90px' }}>
+
+                        </Box>
+                    </Grid>
+                </Grid>
 
 
                 <Grid container spacing={1} justifyContent="center" alignItems="center">
@@ -112,7 +133,7 @@ export default function SignUpBox() {
                     <Grid item>
                         <Link href="userlogin" style={{ display: 'flex', alignItems: 'center' }}>
                             <LoginIcon style={{ marginRight: '8px' }} />
-                            Sign Up
+                            Login
                         </Link>
                     </Grid>
                 </Grid>
@@ -132,3 +153,5 @@ export default function SignUpBox() {
         </div>
     );
 }
+
+
