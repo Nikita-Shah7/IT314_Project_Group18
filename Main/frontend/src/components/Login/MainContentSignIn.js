d5a8b14a58eef0bef4b91914be2558126a367db8
nisarg281 <124192085+nisarg281@users.noreply.github.com>
2023-12-04 16:19:24 +0000

    Path changes

--- Main/frontend/src/components/Login/MainContentSignIn.js
+++ Main/frontend/src/components/Login/MainContentSignIn.js
@@ -71,6 +71,8 @@ export default function SignUpBox() {
 
         if (isEmailValid) {
             setEmail('');
+            toast.success("Login successfully..")
+            navigate('/');
         }
     };
 
